629c8f76cb5f9e8d54b58c371122a65ffd83bfb1
yaocj <2555116@qq.com>
2021-11-21 14:33:04 +0800

    try to save sync position %s because consume err

--- service/endpoint/kafka.go
+++ service/endpoint/kafka.go
@@ -92,8 +92,8 @@ func (s *KafkaEndpoint) Consume(from mysql.Position, rows []*model.RowRequest) e
 		metrics.UpdateActionNum(row.Action, row.RuleKey)
 
 		if global.Cfg().LotDbToMainDb { //不produce message 到kafka
+			return ProcessLotDbToMainDb(from, rows)
 			//to do,to process data to MainB,读取binlog的channel大小可设置成1 (原来代码:queue: make(chan interface{}, 4096),bulk_size=1),这样如果处理数据到MainDb有问题不会丢失数据,当前处理消息位置还要记录防止处理失败丢数据
-			return nil
 		}
 
 		if rule.LuaEnable() {
@@ -234,4 +234,8 @@ func (s *KafkaEndpoint) Close() {
 	if s.client != nil {
 		s.client.Close()
 	}
+}
+
+func ProcessLotDbToMainDb(from mysql.Position, rows []*model.RowRequest) error {
+	return nil
 }
