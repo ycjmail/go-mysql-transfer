026936da1fbb61721a9f6783f5a37fbc82c0fb0c
yaocj <2555116@qq.com>
2021-11-21 15:45:51 +0800

    LotDbToMainDb EndPoint

--- service/endpoint/lotdbtomaindb.go
+++ service/endpoint/lotdbtomaindb.go
@@ -1,42 +1,23 @@
-/*
- * Copyright 2020-2021 the original author(https://github.com/wj596)
- *
- * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * </p>
- */
 package endpoint
 
 import (
+	//"github.com/Shopify/sarama"
 	_ "github.com/siddontang/go-mysql/canal"
 	_ "log"
 	_ "strings"
 	"sync"
 
-	"github.com/Shopify/sarama"
-	"github.com/juju/errors"
 	"github.com/siddontang/go-mysql/mysql"
 
 	"go-mysql-transfer/global"
 	"go-mysql-transfer/metrics"
 	"go-mysql-transfer/model"
-	"go-mysql-transfer/service/luaengine"
 	"go-mysql-transfer/util/logs"
 )
 
 type LotDbToMainDbEndpoint struct {
-	client   sarama.Client
-	producer sarama.AsyncProducer
+	//client   sarama.Client
+	//producer sarama.AsyncProducer
 
 	retryLock sync.Mutex
 }
@@ -78,7 +59,8 @@ func (s *LotDbToMainDbEndpoint) Connect() error {
 }
 
 func (s *LotDbToMainDbEndpoint) Ping() error {
-	return s.client.RefreshMetadata()
+	return nil
+	//return s.client.RefreshMetadata()
 }
 
 func (s *LotDbToMainDbEndpoint) Consume(from mysql.Position, rows []*model.RowRequest) error {
@@ -178,25 +160,25 @@ func (s *LotDbToMainDbEndpoint) Stock(rows []*model.RowRequest) int64 {
 	return int64(len(rows))
 }
 
-func (s *LotDbToMainDbEndpoint) buildMessages(row *model.RowRequest, rule *global.Rule) ([]*sarama.ProducerMessage, error) {
-	kvm := rowMap(row, rule, true)
-	ls, err := luaengine.DoMQOps(kvm, row.Action, rule)
-	if err != nil {
-		return nil, errors.Errorf("lua 脚本执行失败 : %s ", err)
-	}
-
-	var ms []*sarama.ProducerMessage
-	for _, resp := range ls {
-		m := &sarama.ProducerMessage{
-			Topic: resp.Topic,
-			Value: sarama.ByteEncoder(resp.ByteArray),
-		}
-		logs.Infof("topic: %s, message: %s", resp.Topic, string(resp.ByteArray))
-		ms = append(ms, m)
-	}
-
-	return ms, nil
-}
+//func (s *LotDbToMainDbEndpoint) buildMessages(row *model.RowRequest, rule *global.Rule) ([]*sarama.ProducerMessage, error) {
+//	kvm := rowMap(row, rule, true)
+//	ls, err := luaengine.DoMQOps(kvm, row.Action, rule)
+//	if err != nil {
+//		return nil, errors.Errorf("lua 脚本执行失败 : %s ", err)
+//	}
+//
+//	var ms []*sarama.ProducerMessage
+//	for _, resp := range ls {
+//		m := &sarama.ProducerMessage{
+//			Topic: resp.Topic,
+//			Value: sarama.ByteEncoder(resp.ByteArray),
+//		}
+//		logs.Infof("topic: %s, message: %s", resp.Topic, string(resp.ByteArray))
+//		ms = append(ms, m)
+//	}
+//
+//	return ms, nil
+//}
 
 //func (s *LotDbToMainDbEndpoint) buildMessage(row *model.RowRequest, rule *global.Rule) (*sarama.ProducerMessage, error) {
 //	kvm := rowMap(row, rule, false)
@@ -226,12 +208,12 @@ func (s *LotDbToMainDbEndpoint) buildMessages(row *model.RowRequest, rule *globa
 //}
 
 func (s *LotDbToMainDbEndpoint) Close() {
-	if s.producer != nil {
-		s.producer.Close()
-	}
-	if s.client != nil {
-		s.client.Close()
-	}
+	//if s.producer != nil {
+	//	s.producer.Close()
+	//}
+	//if s.client != nil {
+	//	s.client.Close()
+	//}
 }
 
 //ProcessLotDbToMainDb //to do,to process data to MainB,读取binlog的channel大小可设置成1 (原来代码:queue: make(chan interface{}, 4096),bulk_size=1),这样如果处理数据到MainDb有问题不会丢失数据,当前处理消息位置还要记录防止处理失败丢数据
